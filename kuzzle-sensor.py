a82dd420e48be5035a9eb9c73243c830dbd27aaa
Eric Trousset <etrousset@kaliop.com>
2017-10-30 15:56:02 +0100

    multi-sensor: added more help and error checking
    -Added command line args help
    -Added version to program
    -Added Kuzzle connectivity test at startup

--- kuzzle-sensor.py
+++ kuzzle-sensor.py
@@ -13,7 +13,7 @@ from kuzzle.kuzzle import KuzzleIOT
 GPIO_MOTION_SENSOR = 5
 GPIO_BUTTONS = [6, 13, 19, 26]
 
-UID = rpi_get_serial()
+UID = None
 log = logging.getLogger('MAIN')
 
 kuzzle_rfid = None
@@ -29,7 +29,9 @@ def init(args):
     global kuzzle_rfid
     global pn532
     global pi
+    global UID
 
+    UID = rpi_get_serial()
     kuzzle_rfid = KuzzleIOT("NFC_" + UID, "RFID_reader", args.khost, user=args.kuser, pwd=args.kpwd)
     kuzzle_motion = KuzzleIOT("motion_" + UID, "motion-sensor", args.khost, user=args.kuser, pwd=args.kpwd)
     kuzzle_buttons = KuzzleIOT("buttons_{}".format(UID), "button", args.khost, user=args.kuser, pwd=args.kpwd)
@@ -47,6 +49,7 @@ def init(args):
 def logs_init():
     coloredlogs.install(logger=log, fmt='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG,
                         stream=sys.stdout)
+
 
 def on_gpio_changed(gpio, level, tick):
     if gpio in GPIO_BUTTONS:
@@ -68,23 +71,39 @@ def buttons_install():
     for gpio in GPIO_BUTTONS:
         pi.set_mode(gpio, pigpio.INPUT)
         pi.set_pull_up_down(gpio, pigpio.PUD_UP)
-        pi.set_noise_filter(gpio, 50000, 300
+        pi.set_noise_filter(gpio, 300000, 300)
         pi.callback(gpio, pigpio.EITHER_EDGE, on_gpio_changed)
 
-if __name__ == "__main__:
-    parser = argparse.ArgumentParser(description='Kuzzle multi sensor demo')
 
-    parser.add_argument('--pihost', default='localhost',
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Kuzzle IoT - multi sensor demo', prog="kuzzle-iot-demo-multi-device")
+
+    parser.add_argument('--pihost',
+                        default='localhost',
                         help='The host pi to witch the pigpio will connect if user in remote')
-    parser.add_argument('--khost', required=True, help='Kuzzle host')
-    parser.add_argument('--kport', default=7512, type=int, help='Kuzzle port, default is 7512')
-    parser.add_argument('--kuser', help='Kuzzle port, default is 7512')
-    parser.add_argument('--kpwd', help='Kuzzle port, default is 7512')
+    parser.add_argument('--khost', "--kuzzle-host", metavar="KUZZLE_HOST", required=True,
+                        help='Kuzzle host, ip or hostname')
+    parser.add_argument('--kport', metavar="KUZZLE_PORT", default=7512, type=int, help='Kuzzle port, default is 7512')
+    parser.add_argument('--kuser', metavar="KUZZLE_USER", help='Kuzzle user')
+    parser.add_argument('--kpwd', metavar="KUZZLE_PASSWD", help="Kuzzle user's password")
+
+    parser.add_argument('--version', action='version', version='%(prog)s 1.0')
 
     a = parser.parse_args(sys.argv[1:])
-    print(a)
+
     logs_init()
     init(a)
+
+    res = kuzzle_motion.server_info()
+
+    if res:
+        log.debug('Connected to Kuzzle on http://{}:{}, version = {}'.format(
+            a.khost,
+            a.kport,
+            res["serverInfo"]["kuzzle"]["version"])
+        )
+    else:
+        log.warning("Unable to connect to Kuzzle...")
 
     motion_sensor_install()
     buttons_install()
--- kuzzle/kuzzle.py
+++ kuzzle/kuzzle.py
@@ -23,6 +23,7 @@ class KuzzleIOT(object):
     }"""
 
     LOG = logging.getLogger('Kuzzle')
+    JSON_DEC = json.JSONDecoder()
 
     def __init__(self, device_uid, device_type, host='localhost', port='7512',
                  user='', pwd=''):
@@ -39,19 +40,33 @@ class KuzzleIOT(object):
                             level=logging.DEBUG,
                             stream=sys.stdout)
 
-    pass
+    def server_info(self):
+        """
+        Ping Kuzzle to validate where are able to reach
+        """
+
+        url = "http://{}:{}/_serverInfo".format(self.host, self.port)
+        req = requests.get(url=url)
+        res = self.JSON_DEC.decode(req.text)
+        # json.dump(res, sys.stdout, indent=2)
+        if res["status"] == 200:
+            return res["result"]
+        else:
+            self.LOG.critical('Unable to connect to Kuzzle: http://%s:%s', a.khost, a.kport)
+            self.LOG.error(res["error"]["message"])
+            self.LOG.error(res["error"]["stack"])
+            return None
 
     def publish_state(self, state):
         url = "http://{}:{}/{}/{}/_create".format(self.host, self.port, KuzzleIOT.INDEX_IOT,
-                                                   KuzzleIOT.COLLECTION_DEVICE_STATES)
+                                                  KuzzleIOT.COLLECTION_DEVICE_STATES)
         body = {
             "device_id": self.device_uid,
             "device_type": self.device_type,
             "state": state
         }
         req = requests.post(url=url, json=body)
-        json_dec = json.JSONDecoder()
-        res = json_dec.decode(req.text)
+        res = self.JSON_DEC.decode(req.text)
 
         if res['status'] != 200:
             self.LOG.error("Error publishing device state: status = %d", res['status'])
