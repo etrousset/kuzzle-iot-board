b65eaa62ebf1aad62a31754114eb3a3cb0f74865
Eric Trousset <etrousset@kaliop.com>
2018-07-02 14:43:38 +0200

    admin: added possibility to select board type

--- sources/webserver/__init__.py
+++ sources/webserver/__init__.py
@@ -7,14 +7,77 @@ import os
 import subprocess
 import json
 import dbus
+import glob
 
-config_path = None
-config = None
+yaml = YAML()
+
+
+class AdminHTTPServer(HTTPServer):
+    def __init__(self, server_address, RequestHandlerClass, device_info, config_path):
+        super().__init__(server_address, RequestHandlerClass)
+
+        self.config_file = os.path.join(config_path, 'config.yaml')
+        self.device_definitions_path = os.path.join(config_path, 'devices')
+        self.config = self.load_config()
+        self.device_info = device_info
+
+    def get_device_configs(self):
+        configs = []
+
+        files = glob.glob(self.device_definitions_path + "/*")
+
+        for file in files:
+            with open(file) as f:
+                dev_desc_str = f.read()
+            desc =  yaml.load(dev_desc_str)
+
+            configs.append({"name": os.path.basename(file), "desc": desc["description"]})
+        return configs
+
+    def start_admin_server(self):
+        server_path = os.path.dirname(__file__)
+        os.chdir(server_path)
+        self.serve_forever()
+
+    def shutdown_admin_server(self):
+        self.shutdown()
+
+    def load_config(self):
+        with open(self.config_file) as f:
+            config_str = f.read()
+        return yaml.load(config_str)
+
+    def restart_firmware(self):
+        sysbus = dbus.SystemBus()
+        systemd1 = sysbus.get_object('org.freedesktop.systemd1', '/org/freedesktop/systemd1')
+        manager = dbus.Interface(systemd1, 'org.freedesktop.systemd1.Manager')
+        job = manager.RestartUnit('kuzzle-sensor-firmware.service', 'fail')
+
+    def apply_kuzzle_config(self, args):
+        if 'kport' in args.keys():
+            self.config['kuzzle']['port'] = args['kport']
+        if 'khost' in args.keys():
+            self.config['kuzzle']['host'] = args['khost']
+
+        with open(self.config_file, mode='w') as f:
+            yaml.dump(self.config, f)
+
+        self.restart_firmware()
+
+    def apply_device_config(self, args):
+        if 'device_type' in args.keys():
+            self.config['device']['type'] = args['device_type']
+
+        if 'owner' in args.keys():
+            self.config['device']['owner'] = args['owner']
+
+        with open(self.config_file, mode='w') as f:
+            yaml.dump(self.config, f)
+
+        self.restart_firmware()
 
 
 class AdminHTTPRequestHandler(SimpleHTTPRequestHandler):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
 
     def do_POST(self):
         if self.path == '/setup':
@@ -22,7 +85,20 @@ class AdminHTTPRequestHandler(SimpleHTTPRequestHandler):
             c = self.rfile.read(content_length)
             args = uparse.parse_qsl(c)
             args = dict([(x[0].decode('utf-8'), x[1].decode('utf-8')) for x in args])
-            apply_config(args)
+            self.server.apply_kuzzle_config(args)
+
+            self.send_response(301)
+            self.send_header("Location", "http://" + self.headers["Host"] + '/admin')
+            self.end_headers()
+
+        if self.path == '/config':
+            content_length = int(self.headers['Content-Length'])
+            c = self.rfile.read(content_length)
+            args = uparse.parse_qsl(c)
+            args = dict([(x[0].decode('utf-8'), x[1].decode('utf-8')) for x in args])
+
+            print(args)
+            self.server.apply_device_config(args)
 
             self.send_response(301)
             self.send_header("Location", "http://" + self.headers["Host"] + '/admin')
@@ -33,8 +109,13 @@ class AdminHTTPRequestHandler(SimpleHTTPRequestHandler):
             self.send_response(HTTPStatus.OK)
             self.send_header("Content-type", "text/html")
 
-            admin_template = load_admin_template()
-            body = admin_template.merge({'config': config, 'device': {'uid': 'an_uid'}})
+            device_configs = self.server.get_device_configs()
+            print(device_configs)
+            admin_template = self.load_admin_template()
+            body = admin_template.merge(
+                {'config': self.server.config, 'device': {'uid': 'an_uid'}, 'device_configs': device_configs})
+
+            print(body)
 
             self.send_header("Content-length", len(body))
             self.end_headers()
@@ -50,8 +131,8 @@ class AdminHTTPRequestHandler(SimpleHTTPRequestHandler):
             self.send_response(HTTPStatus.OK)
             self.send_header("Content-type", "text/html")
 
-            dashboard_template = load_dashboard_template()
-            body = dashboard_template.merge({'config': json.dumps(config), 'device': device})
+            dashboard_template = self.load_dashboard_template()
+            body = dashboard_template.merge({'config': json.dumps(self.sever.config), 'device': self.server.device_info})
 
             self.send_header("Content-length", len(body))
             self.end_headers()
@@ -94,62 +175,14 @@ class AdminHTTPRequestHandler(SimpleHTTPRequestHandler):
         else:
             super().do_GET()
 
+    @staticmethod
+    def load_admin_template():
+        with open('admin.html.vm') as f:
+            content = f.read()
+        return airspeed.Template(content)
 
-yaml = YAML()
-config_update_event = None
-server_address = ('', 80)
-# AdminHTTPRequestHandler.protocol_version = 'HTTP/1.1'
-httpd = HTTPServer(server_address, AdminHTTPRequestHandler)
-
-
-def restart_firmware():
-    sysbus = dbus.SystemBus()
-    systemd1 = sysbus.get_object('org.freedesktop.systemd1', '/org/freedesktop/systemd1')
-    manager = dbus.Interface(systemd1, 'org.freedesktop.systemd1.Manager')
-    job = manager.RestartUnit('kuzzle-sensor-firmware.service', 'fail')
-
-
-def apply_config(args):
-    if 'kport' in args.keys():
-        config['kuzzle']['port'] = args['kport']
-    if 'khost' in args.keys():
-        config['kuzzle']['host'] = args['khost']
-
-    with open(config_path, mode='w') as f:
-        yaml.dump(config, f)
-
-    restart_firmware()
-
-
-def start_admin_server(device_info, config_path):
-    global device
-
-    globals()['config_path'] = config_path
-    globals()['config'] = load_config()
-    device = device_info
-
-    server_path = os.path.dirname(__file__)
-    os.chdir(server_path)
-    httpd.serve_forever()
-
-
-def shutdown_admin_server():
-    httpd.shutdown()
-
-
-def load_admin_template():
-    with open('admin.html.vm') as f:
-        content = f.read()
-    return airspeed.Template(content)
-
-
-def load_dashboard_template():
-    with open('dashboard.html.vm') as f:
-        content = f.read()
-    return airspeed.Template(content)
-
-
-def load_config():
-    with open(config_path) as f:
-        config_str = f.read()
-    return yaml.load(config_str)
+    @staticmethod
+    def load_dashboard_template():
+        with open('dashboard.html.vm') as f:
+            content = f.read()
+        return airspeed.Template(content)
--- sources/webserver/__main__.py
+++ sources/webserver/__main__.py
@@ -2,5 +2,17 @@ import utils
 from . import *
 
 if __name__ == '__main__':
-    config_path = os.path.abspath('config/config.yaml')
-    start_admin_server({'uid': utils.rpi_get_serial()}, config_path)
+
+    CONFIG_PATH = os.path.abspath('config')
+
+    config_update_event = None
+    SERVER_ADDRESS = ('', 80)
+    # AdminHTTPRequestHandler.protocol_version = 'HTTP/1.1'
+    httpd = AdminHTTPServer(
+        SERVER_ADDRESS,
+        AdminHTTPRequestHandler,
+        {'uid': utils.rpi_get_serial()},
+        CONFIG_PATH
+    )
+
+    httpd.start_admin_server()
